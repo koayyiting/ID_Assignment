6bc06b3c72feb8e157cf6a7fa90d323c07601a94
koayyiting <koayyiting@gmail.com>
2022-02-07 00:26:24 +0800

    CHECKPOINT 2 [TRIED]

--- script.js
+++ script.js
@@ -32,29 +32,22 @@ function includeHTML() {
   }
 }
 
-//[STEP 0]: Make sure our document is A-OK
+
+
 $(document).ready(function () {
-  //what kind of interface we want at the start 
-  const APIKEY = "61d2937accd0211b32089568";
+  const APIKEY = "61ffe45ef701f4600008970e";
   getContacts();
   $("#add-update-msg").hide();
 
-  //[STEP 1]: Create our submit form listener
   $("#contact-submit").on("click", function (e) {
-    //prevent default action of the button 
     e.preventDefault();
 
-    //[STEP 2]: let's retrieve form data
-    //for now we assume all information is valid
-    //you are to do your own data validation
     let contactName = $("#contact-name").val();
     let contactEmail = $("#contact-email").val();
     let contactNumber = $("#contact-number").val();
     let contactSubject = $("#contact-subject").val();
     let contactMessage = $("#contact-msg").val();
 
-    //[STEP 3]: get form values when user clicks on send
-    //Adapted from restdb api
     let jsondata = {
       "name": contactName,
       "email": contactEmail,
@@ -63,12 +56,11 @@ $(document).ready(function () {
       "message": contactMessage
     };
 
-    //[STEP 4]: Create our AJAX settings. Take note of API key
     let settings = {
       "async": true,
       "crossDomain": true,
       "url": "https://interactviedev-2215.restdb.io/rest/contactus",
-      "method": "POST", //[cher] we will use post to send info
+      "method": "POST", 
       "headers": {
         "content-type": "application/json",
         "x-apikey": APIKEY,
@@ -77,25 +69,52 @@ $(document).ready(function () {
       "processData": false,
       "data": JSON.stringify(jsondata),
       "beforeSend": function(){
-        //@TODO use loading bar instead
-        //disable our button or show loading bar
-        $("#contact-submit").prop( "disabled", true);
-        //clear our form using the form id and triggering it's reset feature
+        $("#contact-submit").prop("disabled", true);
         $("#add-contact-form").trigger("reset");
       }
     }
 
-    //[STEP 5]: Send our ajax request over to the DB and print response of the RESTDB storage to console.
     $.ajax(settings).done(function (response) {
       console.log(response);
       
       $("#contact-submit").prop( "disabled", false);
-      
-      //@TODO update frontend UI 
+
       $("#add-update-msg").show().fadeOut(3000);
 
-      //update our table 
       getContacts();
     });
-  });//end click 
+  });
+  
+  function getContacts(limit = 10, all = true) {
+
+    let settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://interactviedev-2215.restdb.io/rest/contactus",
+      "method": "GET",
+      "headers": {
+        "content-type": "application/json",
+        "x-apikey": APIKEY,
+        "cache-control": "no-cache"
+      },
+    }
+
+    $.ajax(settings).done(function (response) {
+      
+      let content = "";
+
+      for (var i = 0; i < response.length && i < limit; i++) {
+
+        content = `${content}<tr id='${response[i]._id}'><td>${response[i].name}</td>
+        <td>${response[i].email}</td>
+        <td>${response[i].number}</td>
+        <td>${response[i].subject}</td>
+        <td>${response[i].message}</td>
+        <td><a href='#' class='delete' data-id='${response[i]._id}'>Del</a></td><td><a href='#update-contact-container' class='update' data-id='${response[i]._id}' data-msg='${response[i].message}' data-name='${response[i].name}' data-email='${response[i].email}'>Update</a></td></tr>`;
+
+      }
+
+      $("#total-contacts").html(response.length);
+    });
+  }
 });
